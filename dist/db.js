d05bd440c6f6485faaf714d78ec0d6c70511ab32
Preetam <sahupreetamkumar@gmail.com>
2025-11-12 10:55:01 +0530

    Update db.js

--- dist/db.js
+++ dist/db.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
     host: process.env.MYSQL_HOST || 'localhost',
     port: Number(process.env.MYSQL_PORT || 3306),
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'root1234',
-    database: process.env.MYSQL_DATABASE || 'rahulshettyacademy',
+    user: process.env.MYSQL_USER || '<username',
+    password: process.env.MYSQL_PASSWORD || 'user_password',
+    database: process.env.MYSQL_DATABASE || 'user_database',
     waitForConnections: true,
     connectionLimit: 10,
     maxIdle: 10,
